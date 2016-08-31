a8ccd4988340722d2f4366fba2ff8d48c4d39f64
Brian Dunlap <briandunlap589@gmail.com>
2016-08-31 18:18:26 -0400

    Add relationship with category to dishes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160831220853) do
+ActiveRecord::Schema.define(version: 20160831221728) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -21,8 +21,9 @@ ActiveRecord::Schema.define(version: 20160831220853) do
   create_table "dishes", force: :cascade do |t|
     t.string   "name"
     t.string   "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
 
 end
