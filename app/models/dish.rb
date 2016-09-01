1a5c67ff231ea179b9f2df72f0a96390afa5a03e
Brian Dunlap <briandunlap589@gmail.com>
2016-09-01 16:54:09 -0400

    Add method to calculate cost after tax for dishes

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,4 +1,15 @@
 class Dish < ApplicationRecord
   belongs_to :category
   scope :ordered_by_category, -> { all.order("category_id ASC, price ASC") }
+
+  def cost_after_tax
+    total = self.price.to_f * (1 + tax_rate)
+    '%.2f' % total.round(2)
+  end
+
+  private
+
+  def tax_rate
+    0.07
+  end
 end
